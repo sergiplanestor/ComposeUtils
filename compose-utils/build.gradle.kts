81ac36910f80a5b2b8eb8d6c84fa2e837badb645
splanes <sergio.planes@adevinta.com>
2022-02-01 00:05:31 +0100

    Publication Test - 6

--- compose-utils/build.gradle.kts
+++ compose-utils/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    libPlugins(/*GradlePlugin.Publish*/).forEach(::id)
+    libPlugins(GradlePlugin.Publish).forEach(::id)
 }
 
 android {
@@ -32,22 +32,4 @@ dependencies {
     test()
 }
 
-/*
-afterEvaluate {
-    publishing {
-        publications {
-            release {
-                from(releaseComponent)
-                groupId = artifactGroup
-                artifactId = artifactId(isDebug = false)
-                version = artifactVersion
-            }
-            debug {
-                from(debugComponent)
-                groupId = artifactGroup
-                artifactId = artifactId(isDebug = true)
-                version = artifactVersion
-            }
-        }
-    }
-}*/
+publish()
